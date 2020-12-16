ccbabb9ebfa70c27b872af8a84eca5c839f93970
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-12-16 12:16:27 +0200

    update drone

--- src/app.js
+++ src/app.js
@@ -1,43 +1,40 @@
 /* eslint-disable no-unused-vars, no-undef, no-warning-comments */
 import {promisify} from 'util';
 import {createLogger} from '@natlibfi/melinda-backend-commons';
-import {mongoFactory, COMMON_JOB_STATES} from '@natlibfi/melinda-record-link-migration-commons';
+import {eratuontiFactory, mongoFactory, COMMON_JOB_STATES} from '@natlibfi/melinda-record-link-migration-commons';
 import {readBlobsFromEratuonti} from './interfaces/eratuonti';
-export default async function ({
-  apiUrl, apiUsername, apiPassword, apiClientUserAgent, mongoUrl, amqpUrl
-}) {
+
+export default async function ({apiUrl, apiUsername, apiPassword, apiClientUserAgent, mongoUrl}) {
   const logger = createLogger();
-  const eratuontiConfig = {apiUrl, apiUsername, apiPassword, apiClientUserAgent};
   const mongoOperator = await mongoFactory(mongoUrl);
+  const eratuontiOperator = eratuontiFactory({apiUrl, apiUsername, apiPassword, apiClientUserAgent}); // eslint-disable-line no-unused-vars
   const setTimeoutPromise = promisify(setTimeout);
   logger.log('info', 'Melinda-eratuonti-watcher-link-migration has started');
 
   return check();
 
   async function check(wait) {
-    if (wait) {
-      await setTimeoutPromise(3000);
-      return check();
-    }
-
     await checkJobsInState(COMMON_JOB_STATES.PENDING_ERATUONTI);
 
-    // Loop
-    return check(true);
+    throw new Error('Checks done - Shutting down!');
   }
 
   async function checkJobsInState(state) {
-    const job = await mongoOperator.getOne(state);
+    const promises = [];
+    const cursor = await mongoOperator.getAll(state);
+    await cursor.forEach(handlejob);
+    await Promise.all(promises);
 
-    if (job) {
+    async function handlejob(job) {
+      logger.log('debug', `Job id: ${job.jobId}`);
       // logger.log('info', JSON.stringify(job, undefined, 2));
       // logger.log('debug', JSON.stringify(job.blobIds));
 
       if (job.blobIds.length < 1) { // eslint-disable-line functional/no-conditional-statement
-        await mongoOperator.setState({jobId: job.jobId, state: COMMON_JOB_STATES.ERROR});
+        return promises.push(await mongoOperator.setState({jobId: job.jobId, state: COMMON_JOB_STATES.ERROR})); // eslint-disable-line functional/immutable-data
       }
 
-      await readBlobsFromEratuonti(job, mongoOperator, eratuontiConfig);
+      return promises.push(readBlobsFromEratuonti(job, mongoOperator, eratuontiOperator)); // eslint-disable-line functional/immutable-data
     }
   }
 }
--- src/interfaces/eratuonti.js
+++ src/interfaces/eratuonti.js
@@ -1,44 +1,37 @@
 /* eslint-disable no-unused-vars */
 import {promisify} from 'util';
 import {createLogger} from '@natlibfi/melinda-backend-commons';
-import {eratuontiFactory, COMMON_JOB_STATES, HARVESTER_JOB_STATES} from '@natlibfi/melinda-record-link-migration-commons';
+import {COMMON_JOB_STATES, HARVESTER_JOB_STATES} from '@natlibfi/melinda-record-link-migration-commons';
 import {BLOB_STATE} from '@natlibfi/melinda-record-import-commons';
-import moment from 'moment';
 
-export async function readBlobsFromEratuonti(job, mongoOperator, {apiUrl, apiUsername, apiPassword, apiClientUserAgent}) {
+export async function readBlobsFromEratuonti(job, mongoOperator, eratuontiOperator) {
   const logger = createLogger();
-  const setTimeoutPromise = promisify(setTimeout);
   const {jobId, jobConfig, blobIds} = job;
-  const {linkDataHarvesterApiProfileId} = jobConfig;
-  const eratuontiOperator = eratuontiFactory({apiUrl, apiUsername, apiPassword, apiClientUserAgent, linkDataHarvesterApiProfileId}); // eslint-disable-line no-unused-vars
   logger.log('info', `Reading eratuonti blobs from job: ${jobId}`);
   logger.log('info', `Blobs: ${blobIds}`);
 
-  const needAction = await readBlobs(blobIds);
+  const {needAction, done} = await readBlobs(blobIds);
+
   if (needAction) {
-    await handleReset();
+    // All blobs handled as far as they could be
+    if (done === blobIds.length) {
+      await handleReset();
+      return;
+    }
     return;
   }
 
-  await mongoOperator.setState({jobId, state: COMMON_JOB_STATES.DONE});
-  return;
+  // All fine and dandy
+  if (done === blobIds.length) {
+    await mongoOperator.setState({jobId, state: COMMON_JOB_STATES.DONE});
+    return;
+  }
 
-  async function readBlobs(blobIds, needRestart = false, wait = false) {
-    if (wait) {
-      await setTimeoutPromise(3000);
-      return readBlobs(blobIds);
-    }
-
+  async function readBlobs(blobIds = [], needRestart = false, done = 0) {
     const [id, ...rest] = blobIds;
     if (id === undefined) {
-      if (needRestart) {
-        return true;
-      }
-
-      return false;
+      return {needAction: needRestart, done};
     }
-
-    logger.log('debug', `Blob id: ${id}`);
 
     const result = await eratuontiOperator.readBlob(id);
 
@@ -77,50 +70,59 @@ export async function readBlobsFromEratuonti(job, mongoOperator, {apiUrl, apiUse
     // console.log(result); // eslint-disable-line no-console
 
     /*Blob states: PENDING_TRANSFORMATION, TRANSFORMATION_IN_PROGRESS, TRANSFORMATION_FAILED, TRANSFORMED, PROCESSED, ABORTED */
+    // logger.log('debug', `Blob id: ${id}`);
     if (result.state === BLOB_STATE.PENDING_TRANSFORMATION) {
-      logger.log('info', 'Blob is pending transformation!');
+      logger.log('silly', 'Blob is pending transformation!');
       logger.log('silly', JSON.stringify(result, undefined, 2));
-      return readBlobs([id, ...rest], needRestart, true);
+      return readBlobs(rest, needRestart, true, done);
     }
 
     if (result.state === BLOB_STATE.TRANSFORMATION_IN_PROGRESS) {
-      logger.log('info', 'Blob is in transformation!');
+      logger.log('silly', 'Blob is in transformation!');
       logger.log('silly', JSON.stringify(result, undefined, 2));
-      return readBlobs([id, ...rest], needRestart, true);
+      return readBlobs(rest, needRestart, true, done);
     }
 
     if (result.state === BLOB_STATE.TRANSFORMED) {
-      logger.log('info', 'Blob has been transformed!');
+      logger.log('silly', 'Blob has been transformed!');
       logger.log('silly', JSON.stringify(result, undefined, 2));
-      return readBlobs([id, ...rest], needRestart, true);
+      return readBlobs(rest, needRestart, true, done);
+    }
+
+    if (result.state === BLOB_STATE.PROCESSING) {
+      logger.log('silly', 'Blob is in processing!');
+      logger.log('silly', JSON.stringify(result, undefined, 2));
+      return readBlobs(rest, needRestart, true, done);
     }
 
     if (result.state === BLOB_STATE.PROCESSED) {
-      await logger.log('info', 'Blob prosessed!');
-      const done = handleProsessed(result);
+      logger.log('debug', `Blob id: ${id}`);
+      logger.log('info', 'Blob prosessed!');
+      const notDone = handleProsessed(result);
 
-      if (!done) {
-        logger.log('debug', JSON.stringify(result, undefined, 2));
-        return readBlobs(rest, needRestart || !done);
+      if (!notDone) {
+        logger.log('silly', JSON.stringify(result, undefined, 2));
+        return readBlobs(rest, needRestart, done);
       }
-
-      return readBlobs(rest, !done);
+      const newDone = done + 1;
+      return readBlobs(rest, !notDone, newDone);
     }
 
     if (result.state === BLOB_STATE.TRANSFORMATION_FAILED) {
       logger.log('info', 'Blob transformation failed!');
       logger.log('debug', JSON.stringify(result, undefined, 2));
-
-      return readBlobs(rest, true);
+      const newDone = done + 1;
+      return readBlobs(rest, true, newDone);
     }
 
     if (result.state === BLOB_STATE.ABORTED) {
+      logger.log('debug', `Blob id: ${id}`);
       logger.log('info', 'Blob ABORTED!');
-      await mongoOperator.setState({jobId, state: COMMON_JOB_STATES.ABORTED});
-      return readBlobs(rest, needRestart);
+      const newDone = done + 1;
+      return readBlobs(rest, true, newDone);
     }
 
-    return readBlobs(rest, needRestart, true);
+    return readBlobs(rest, needRestart, done);
 
     function handleProsessed(result) {
       const {importResults} = result.processingInfo;
@@ -152,7 +154,7 @@ export async function readBlobsFromEratuonti(job, mongoOperator, {apiUrl, apiUse
     }
 
     if (jobConfig.linkDataHarvestSearch.type === 'finto') {
-      await mongoOperator.setState({jobId, state: HARVESTER_JOB_STATES.PENDING_OAI_PMH_HARVESTER});
+      await mongoOperator.setState({jobId, state: HARVESTER_JOB_STATES.PENDING_FINTO_HARVESTER});
       return;
     }
 
