4d6c67d0bfd03734c9577eba464dcef5c4cb48c4
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-12-20 19:14:28 +0200

    restore defaults

--- src/config.js
+++ src/config.js
@@ -7,4 +7,4 @@ export const apiPassword = readEnvironmentVariable('API_PASSWORD');
 export const apiClientUserAgent = readEnvironmentVariable('API_CLIENT_USER_AGENT', {defaultValue: '_RECORD-UPDATE-HANDLER'});
 
 // Mongo variables to job
-export const mongoUrl = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://127.0.0.1:27018/db'});
+export const mongoUrl = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://127.0.0.1:27017/db'});
